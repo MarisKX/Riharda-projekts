2df3849171a4828003d64a0d68793fba3b94e0f2
MarisKX <maris.kurpnieks@gmail.com>
2024-05-18 18:16:48 +0200

    Minor bug fixes

--- initViewHandler.js
+++ initViewHandler.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const playerNameInput = document.getElementById('player');
   document.getElementById('initView').addEventListener('click', function() {
     var initView = this;
     initView.classList.add('hidden'); // Start the fade-out effect
@@ -7,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
         initView.style.display = 'none'; // Hide the initView completely
         document.getElementById('playerEntryContent').style.display = 'flex'; // Show the other content
+        playerNameInput.focus();
     }, 1200); // This should match the duration of the CSS transition
   });
 });
